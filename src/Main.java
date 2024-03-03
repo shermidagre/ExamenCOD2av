d317ef03ce497ec99b6c70c6621d7a13f4692eb1
Damian Nogueiras <damian@danielcastelao.org>
2024-03-03 17:05:44 +0100

    version 0.1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+/**
+ * Clase Principal
+ */
 public class Main {
+    /**
+     * Metodo principal
+     * @param args no se utilizan
+     */
     public static void main(String[] args) {
-
+        // Imprime un mensaje
         System.out.println("Hola Examen!");
     }
 }
